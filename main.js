4cf54827e884e681d1dbf4966eb21ab469e4d7ac
Andrew <andrewhoonchang97@gmail.com>
2023-08-15 20:46:38 -0700

    Added more options and removed others for randomly generated lists

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ const animals = [
     'Squirrel', 'Ostrich', 'Chameleon', 'Peacock', 'Raccoon',
     'Orangutan', 'Salamander', 'Alpaca', 'Tarantula',
     'Chimpanzee', 'Gecko', 'Gazelle', 'Moose',
-    'Hyena', 'Capybara', 'Platypus', 'Walrus'
+    'Hyena', 'Capybara', 'Platypus', 'Walrus', 'Slime', 'Spirits',
+    'Ghost', 'Mushroom', 'Fairies', 'Mouse', 'Cat', 'Dog', 'Rabbit', 
+    'Duck', 'Bear', 'Pokemon', 'Favorite Anime Character'
 ]
 const locations = [
     'Forest', 'Beach', 'City', 'Mountains', 'Space',
@@ -18,7 +20,9 @@ const locations = [
     'Savannah', 'Oasis', 'Cottage', 'Harbor', 'Glacier',
     'Subway', 'Carnival', 'Skyscraper', 'Pirate Ship', 'Rainforest',
     'Cathedral', 'Amusement Park', 'Space Station', 'Haunted House', 'Ski Resort',
-    'Moon', 'Space Colony', 'Circus Tent', 'Underground Bunker', 'Library'
+    'Moon', 'Space Colony', 'Circus Tent', 'Underground Bunker', 'Library',
+    'Cozy Living Room', 'Messy Bedroom', 'Quaint Cafe', 'Overgrown Train Station', 'Floating Castle',
+    'Movie Inspired Scene', 'Pallet Town'
 ]
 const items = [
     'Umbrella', 'Book', 'Telescope', 'Camera', 'Palette',
@@ -30,27 +34,9 @@ const items = [
     'Crown', 'Mask', 'Globe', 'Treasure Map', 'Ink Bottle',
     'Music Sheet', 'Scepter', 'Spyglass', 'Wand', 'Mechanical Gear',
     'Mystical Amulet', 'Pocket Knife', 'Stethoscope', 'Golden Key', 'Puzzle Box',
-    'Paint Tube', 'Pirate Hat',
-    'Coffee Mug', 'Sketchbook', 'Pencil Set', 'Watercolor Palette', 'Acrylic Paints',
-    'Sculpting Clay', 'Easel Stand', 'Canvas Roll', 'Coloring Markers', 'Oil Paint Set',
-    'Charcoal Pencils', 'Artist\'\s Apron', 'Pastel Chalks', 'Drawing Board', 'Art Journal',
-    'Calligraphy Pen', 'Craft Knife', 'Ruler', 'Drafting Compass', 'Graphite Pencils',
-    'Painting Brushes', 'Wooden Mannequin', 'Lightbox', 'Palette Knife', 'Ink Bottles',
-    'Canvas Panels', 'Sketching Charcoal', 'Watercolor Brushes', 'Clay Tools', 'Chalkboard',
-    'Color Mixing Guide', 'Drawing Paper', 'Stretched Canvas', 'Erasers', 'Oil Paint Medium',
-    'Palette Cups', 'Sponges', 'Masking Tape', 'Fixative Spray', 'Canvas Stretching Pliers',
-    'Color Wheel', 'Art Satchel', 'Sculpting Tools', 'Drafting Paper', 'Gouache Paints',
-    'Acrylic Mediums', 'Drawing Easel', 'Stump and Tortillon Set', 'Tracing Paper', 'Painting Palette',
-    'Artists\'\ Gloves', 'Linen Canvas', 'Blending Stumps', 'Canvas Varnish', 'Paper Cutter',
-    'Perspective Grids', 'Staple Gun', 'Watercolor Paper Blocks', 'Plein Air Kit', 'Charcoal Paper',
-    'Spray Fixative', 'Art Storage Box', 'Blending Brushes', 'Color Shapers', 'French Easel',
-    'Encaustic Wax', 'Masonite Panels', 'Bamboo Brushes', 'Oil Brush Cleaner', 'Canvas Pad',
-    'Manikin Hand', 'Gesso Primer', 'Foam Brushes', 'Bristle Brushes', 'Drafting T-Square',
-    'Wooden Palette', 'Cotton Canvas Rolls', 'Chamois Cloth', 'Sculpture Armature', 'Drawing Mannequin',
-    'Soft Pastels', 'Canvas Apron', 'Acrylic Brushes', 'Watercolor Ground', 'Oil Pastels',
-    'Turpentine Solvent', 'Brush Holder', 'Acrylic Spray Varnish', 'Gel Medium', 'Landscape Brush Set',
-    'Canvas Stretcher Bars', 'Printmaking Tools', 'Block Printing Ink', 'Sculpture Wire', 'Rag Paper',
-    'Bristle Brushes', 'Soft Erasers', 'Oil Pastel Paper', 'Painting Varnish', 'Gel Pens'
+    'Paint Tube', 'Pirate Hat', 'Coffee Mug', 'Sketchbook', 'cheese', 'Magic Wand', 'Flower Bouquet',
+    'Stuffed Animal', 'Cake', 'Train', 'Toys', 'Spatula', 'Bicycle', 'Hot Air Balloon', 'Lollipop', 'Star-shaped Sunglasses',
+    'Jellyfish Lanterns', 'Mushroom Teapot', 'Flower Crown Wreath'
 ]
 
 const generateButton = document.querySelector('.generate');
